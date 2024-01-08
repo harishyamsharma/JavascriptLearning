210663f1d559c1b2f297b9fe767c639f18a1b6b7
Hari <sharmahari4157@gmail.com>
2024-01-08 22:32:20 +0530

    test

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 1456
+let accountEmail ="sharmahari4157@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur" // do not use the variable without let & var
+let accountState;
+
+// accountId = 2 // Not Allowed
+
+accountEmail = "hari@gmail.com"
+accountPassword ="54321"
+accountCity = "Mathura"
+ 
+console.log(accountId);
+
+/*
+Prefer not use to var
+because of issue in the block scope & functional scope 
+*/
+
+console.log([accountEmail, accountId, accountPassword, accountCity, accountState])   
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])   // 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all the js code as thr newer version 
+
+// alert(3+3)  // We are using node js, not browser 
+
+console.log(3+3); console.log("Hari")  // Code make proper readability , This is not a good practice 
+
+console.log(3+3) // This is the proper code.
+
+let name = "Hari"    // String Variable
+let age = 28         // numbers
+//let userLoggedIn = True    //boolean 
+
+
+// All are the datatypes given below
+// Strings  =>  " "
+// numbers  => 2 to power 53
+// boolean  => True/False
+// bigint  => 12345678987456
+// null     => Standerd value
+// undefined =>
+// Symbol => unique
+
+
+//objects
+
+
+console.log(typeof "Hari")
