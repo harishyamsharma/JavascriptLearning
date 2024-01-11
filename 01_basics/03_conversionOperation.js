e7193c7f71c1e5b6b10dbbab5e55267c36c7ed17
Hari <sharmahari4157@gmail.com>
2024-01-11 21:12:39 +0530

    Type of Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+
+
+let score = 33 
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)           // Print NaN : Noa a Number ( This is also spacial data type )
+
+
+
+// 33 => 33
+// "33" => string
+// "33xyz" => NaN
+// null => 0
+// undefined => NaN (Not a number)
+// true => 1 , false => 0
+
+
+let isLoggedIn = "Hari"
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
+
+// 1 => true 0 => false
+// "" => false
+// "Hari" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
