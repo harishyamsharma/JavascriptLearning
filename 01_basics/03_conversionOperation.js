efb2644c4b0f9a79681e06e52af555dc33b2daa3
Hari <sharmahari4157@gmail.com>
2024-01-15 20:52:48 +0530

    File Modified

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -34,3 +34,46 @@ let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
 
 
+
+
+let value = 3
+let nagValue = -value
+console.log(nagValue);
+
+// Arithmatic conversion
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+// let str1 = "hello"
+// let str2 = " Hari "
+// let str3 = str1 + str2
+// console.log (str3);
+
+// console.log( "2" + 1 );
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+// Prefix & Postfix incrementor 
+
+let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+// gameCounter--;
+console.log(gameCounter);
+
+
+
+
